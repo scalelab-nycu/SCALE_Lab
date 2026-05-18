9475d4b20c3c0c7439632ec510758e2005c6f07b
scalelab-nycu <scalelab.nycu@gmail.com>
2026-05-18 12:51:52 +0800

    Remove Contact and swap Member and Publication nav order

--- assets/script.js
+++ assets/script.js
@@ -12,8 +12,8 @@ const HEADER_HTML = `
     <ul id="menu">
       <li><a data-page="research" href="research.html">Research</a></li>
       <li><a data-page="advisor" href="advisor.html">Advisor</a></li>
-      <li><a data-page="publication" href="publication.html">Publication</a></li>
       <li><a data-page="people" href="people.html">Member</a></li>
+      <li><a data-page="publication" href="publication.html">Publication</a></li>
       <li><a data-page="course" href="course.html">Course</a></li>
     </ul>
   </nav>
